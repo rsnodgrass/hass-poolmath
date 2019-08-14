16b8ca1806d3127d409089c3ca6cf29928d34a33
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-08-14 00:32:56 -0700

    direct Tag iteration

--- custom_components/poolmath/sensor.py
+++ custom_components/poolmath/sensor.py
@@ -129,15 +129,16 @@ class PoolMathClient():
             sensor_type = 'unknown'
             state = 'unknown'
             
+            # bs4.element.Tag
             LOG.warning(f"type = {type(entry)}")
 
             # FIXME: there must be a better way to traverse the sub-trees than 
             # creating a new parsed soup.
-            soup = BeautifulSoup(f" {entry}", 'html.parser')
-            for div in soup.find('div'):
+#            soup = BeautifulSoup(entry.contents, 'html.parser')
+            for div in entry.children: 
                 LOG.warning(f"Div {div} ({div['class']}) {div.string}")
                 if div['class'] == 'bold':
-                    state = div.string
+                    state = div.contents
                 else:
                     sensor_type = div['class']
             
