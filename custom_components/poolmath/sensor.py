de1034804fc22fda2c1fd876597ed61f9c0b58b1
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-08-20 21:53:57 -0700

    text

--- custom_components/poolmath/sensor.py
+++ custom_components/poolmath/sensor.py
@@ -130,11 +130,12 @@ class PoolMathClient():
             state = entry.contents[1].string
 
             self.log_test(entry.contents[3])
-            self.log_test(entry.contents[3].string)
-            self.log_test(entry.contents[3].string.decode('utf-8'))
-            self.log_test(entry.contents[3].string.decode('utf-8').lower)
+            self.log_test(entry.contents[3].text)
+#            self.log_test(entry.contents[3].text.decode('utf-8'))
+#            self.log_test(entry.contents[3].string.decode('utf-8').lower)
             
-            sensor_type = entry.contents[3].string.decode('utf-8').lower
+ #           sensor_type = entry.contents[3].string.decode('utf-8').lower
+            sensor_type = entry.contents[3].text
 
             LOG.warn(f"Found sensor type '{sensor_type}' = {state}")
             sensor = self.get_sensor(sensor_type)
